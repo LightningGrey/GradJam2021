f29507239180767ef08ed6ff661c5426e1135c19
Steph <stephane.gagnon00@gmail.com>
2021-12-10 16:05:56 -0500

    main menu

---
+++ Assets/Scripts/Managers/MenuManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField] private GameObject instructionsPanel;
+
+    public void OnPlayButton()
+    {
+        SceneManager.LoadScene("Test Level 2");
+    }
+
+    public void OnInstructionsButton()
+    {
+        instructionsPanel.SetActive(true);
+    }
+
+    public void OnInstructionsCloseButton()
+    {
+        instructionsPanel.SetActive(false);
+    }
+
+    public void OnExitButton()
+    {
+        Application.Quit();
+    }
+}
